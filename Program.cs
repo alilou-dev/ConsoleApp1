c569106737927876e85851ea87fdf0452efc0ef9
ALI <raidali95@yahoo.com>
2024-06-03 16:18:57 +0200

    test github action

--- Program.cs
+++ Program.cs
@@ -3,3 +3,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("new commit" +
                   "fdsfsdfsd");
+Console.WriteLine("test after github action" +
+                  "");                  
